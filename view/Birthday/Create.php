1ed72af690f4a3c5fbd74822e81d5c2e8c8d8cc9
jop de winter <99039923@mydavinci.nl>
2017-03-23 10:55:00 +0100

    create input gemaakt

---
+++ view/Birthday/Create.php
@@ -0,0 +1,13 @@
+<div class="container">
+	<form action="<?= URL ?>Birthday/createSave" method="post">
+	
+		<input type="text" name="dag" placeholder="dag">
+		<input type="text" name="maand" placeholder="maand">
+        <input type="text" name="jaar" placeholder="jaar">
+        <input type="text" name="naam" placeholder="jaar">
+
+		<input type="submit" value="Verzenden">
+	
+	</form>
+
+</div>
